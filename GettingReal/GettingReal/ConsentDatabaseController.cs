d504fc2b8ee5fb681d63d82b3c3901bec092bccf
Kasper Schiang <kasp5728@edu.eal.dk>
2017-12-05 16:34:22 +0100

    Der blev implementered ExecuteNonQuery i ConsentDataBase + Test.

--- GettingReal/GettingReal/ConsentDatabaseController.cs
+++ GettingReal/GettingReal/ConsentDatabaseController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace GettingReal
 {
@@ -10,8 +12,38 @@ namespace GettingReal
 	{
         private static string connectionString = 
             "Server=EALSQL1.eal.local; " +
-            "Database=DB2017_B14; " +
-            "User Id=USER_B14; " +
-            "Password=SesamLukOp_14;"; 
-	}
+            "Database=DB2017_B20; " +
+            "User Id=USER_B20; " +
+            "Password=SesamLukOp_20;";
+        public static void ExecuteNonQuery(string storedProcedure, Dictionary<string, object> parameterInput)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+                    SqlCommand command = new SqlCommand(storedProcedure, con);
+                    command.CommandType = CommandType.StoredProcedure;
+                    foreach (KeyValuePair<string, object> parameter  in parameterInput)
+                    {
+                        command.Parameters.Add(new SqlParameter("@" + parameter.Key, parameter.Value));
+
+                    }
+
+                    command.ExecuteNonQuery();
+
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine("ERROR! " + e.Message);
+                }
+            }
+
+        }
+    }
+
+   
+    
+
+
 }
--- GettingReal/UnitTests/UnitTest1.cs
+++ GettingReal/UnitTests/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using GettingReal;
+using System.Collections.Generic;
 namespace UnitTests
 {
 	[TestClass]
@@ -9,6 +10,15 @@ namespace UnitTests
 		[TestMethod]
 		public void TestMethod1()
 		{
+            Dictionary<string, object> parameter = new Dictionary<string, object>();
+            parameter.Add("OwnerID", 1);
+            parameter.Add("PetName", "Søren");
+            parameter.Add("PetType", "Dog");
+            parameter.Add("PetBreed", "Corgi");
+            parameter.Add("PetDOB", "14.05.2016");
+            parameter.Add("PetWeight", 14.1d);
+            ConsentDatabaseController.ExecuteNonQuery("InsertPet", parameter);
+            
 		}
 	}
 }
