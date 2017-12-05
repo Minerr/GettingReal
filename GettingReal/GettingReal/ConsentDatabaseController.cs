ba3c61c090cba806a1b6bedcc49f857a81b6668e
Minerr <jack.corp@hotmail.com>
2017-12-05 16:38:23 +0100

    Formated ConsentDatabaseController

--- GettingReal/GettingReal/ConsentDatabaseController.cs
+++ GettingReal/GettingReal/ConsentDatabaseController.cs
@@ -10,40 +10,40 @@ namespace GettingReal
 {
 	public static class ConsentDatabaseController
 	{
-        private static string connectionString = 
-            "Server=EALSQL1.eal.local; " +
-            "Database=DB2017_B20; " +
-            "User Id=USER_B20; " +
-            "Password=SesamLukOp_20;";
-        public static void ExecuteNonQuery(string storedProcedure, Dictionary<string, object> parameterInput)
-        {
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                try
-                {
-                    con.Open();
-                    SqlCommand command = new SqlCommand(storedProcedure, con);
-                    command.CommandType = CommandType.StoredProcedure;
-                    foreach (KeyValuePair<string, object> parameter  in parameterInput)
-                    {
-                        command.Parameters.Add(new SqlParameter("@" + parameter.Key, parameter.Value));
+		private static string connectionString =
+			"Server=EALSQL1.eal.local; " +
+			"Database=DB2017_B20; " +
+			"User Id=USER_B20; " +
+			"Password=SesamLukOp_20;";
 
-                    }
+		public static void ExecuteNonQuery(string storedProcedure, Dictionary<string, object> parameterInput)
+		{
+			using(SqlConnection con = new SqlConnection(connectionString))
+			{
+				try
+				{
+					con.Open();
 
-                    command.ExecuteNonQuery();
+					SqlCommand command = new SqlCommand(storedProcedure, con);
+					command.CommandType = CommandType.StoredProcedure;
+					foreach(KeyValuePair<string, object> parameter in parameterInput)
+					{
+						command.Parameters.Add(new SqlParameter("@" + parameter.Key, parameter.Value));
+					}
 
-                }
-                catch (SqlException e)
-                {
-                    Console.WriteLine("ERROR! " + e.Message);
-                }
-            }
+					command.ExecuteNonQuery();
+				}
+				catch(SqlException e)
+				{
+					Console.WriteLine("ERROR! " + e.Message);
+				}
+			}
 
-        }
-    }
+		}
+	}
 
-   
-    
+
+
 
 
 }
