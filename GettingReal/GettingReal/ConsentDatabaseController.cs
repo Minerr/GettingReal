b3bfb00617a4439e4d011f70fa2f8a656f9803ba
Kasper Schiang <kasp5728@edu.eal.dk>
2017-12-05 15:32:01 +0100

    Added connectionString to the ConsentDatabase

--- GettingReal/GettingReal/ConsentDatabaseController.cs
+++ GettingReal/GettingReal/ConsentDatabaseController.cs
@@ -8,6 +8,10 @@ namespace GettingReal
 {
 	public static class ConsentDatabaseController
 	{
-
+        private static string connectionString = 
+            "Server=EALSQL1.eal.local; " +
+            "Database=DB2017_B14; " +
+            "User Id=USER_B14; " +
+            "Password=SesamLukOp_14;"; 
 	}
 }
