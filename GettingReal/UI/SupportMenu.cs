32e45fe4462d9454ea9c5821fb97f919ddc5a62d
Benjamin Faarvang Grand <benj8147@edu.eal.dk>
2017-12-15 12:57:45 +0100

    Rettet lidt i SupportMenu

--- GettingReal/UI/SupportMenu.cs
+++ GettingReal/UI/SupportMenu.cs
@@ -6,36 +6,55 @@ using System.Threading.Tasks;
 
 namespace UI
 {
-	public class SupportMenu : IMenu
-	{
-		private static SupportMenu instance;
-		private SupportMenu() { }
-		public static SupportMenu Instance
-		{
-			get
-			{
-				if(instance == null)
-				{
-					instance = new SupportMenu();
-				}
+    public class SupportMenu : IMenu
+    {
+        private static SupportMenu instance;
+        private SupportMenu() { }
+        public static SupportMenu Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new SupportMenu();
+                }
 
-				return instance;
-			}
+                return instance;
+            }
+        }
+
+        public void ShowMenu()
+        {
+            SupportMainMenu();
+        }
+
+        public void HandleInput(string input)
+        {
+            switch (input)
+            {
+                case "1":
+                    ShowAllPermissions();
+                    break;
+                default:
+					Console.WriteLine("Wrong choice, try again");
+                    break;
+            }
+			Console.WriteLine("Press any key to continue");
+			Console.ReadKey();
 		}
 
-		public void ShowMenu()
-		{
-			ShowAllPermissions();
-		}
+        private void ShowAllPermissions()
+        {
+            Console.WriteLine("This is support menu");
+        }
 
-		public void HandleInput(string input)
-		{
-			
-		}
+        private void SupportMainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Support Menu");
+            Console.WriteLine("");
+            Console.WriteLine("1. Show all permissions");
 
-		private void ShowAllPermissions()
-		{
-			Console.WriteLine("This is support menu");
-		}
-	}
+        }
+    }
 }
